6ba72546732bebbb57e9018ca612e7b6756afdb1
Nazmul Islam <150185244+Nazmul1005@users.noreply.github.com>
2026-04-05 21:34:59 +0600

    Implement swap calculation for max and min values

--- third.py
+++ third.py
@@ -1,1 +1,17 @@
-print("Nazmul Islam")
+
+import sys
+
+n = int(sys.stdin.readline())
+a = list(map(int, sys.stdin.readline().split()))
+
+max_val = max(a)
+min_val = min(a)
+
+max_idx = a.index(max_val)
+min_idx = n - 1 - a[::-1].index(min_val)
+
+swaps = max_idx + (n - 1 - min_idx)
+if max_idx > min_idx:
+    swaps -= 1
+
+print(swaps)
