debe8c5800a415c34c18b97dea365d236922846f
Nazmul Islam <150185244+Nazmul1005@users.noreply.github.com>
2026-04-05 21:35:47 +0600

    Implement logic to find smallest and largest numbers
    Refactor code to read input and calculate smallest and largest numbers based on given conditions.

--- first.py
+++ first.py
@@ -1,8 +1,33 @@
-a = 6
-print(a)
+m, s = map(int, input().split())
 
-print("Hello, World!")
 
-print("HEY! mY name is Nazmul")
+if s == 0:
+    if m == 1:
+        print("0 0")
+    else:
+        print("-1 -1")
+elif s > 9 * m:
+    print("-1 -1")
+else:
+    
+    smallest = []
+    remaining = s
+    for i in range(m):
+        for d in range(0 if i > 0 else 1, 10):
+           
+            left = m - i - 1
+            if remaining - d <= 9 * left:
+                smallest.append(d)
+                remaining -= d
+                break
 
+   
+    largest = []
+    remaining = s
+    for i in range(m):
+        d = min(9, remaining)
+        largest.append(d)
+        remaining -= d
+
+    print(''.join(map(str, smallest)), ''.join(map(str, largest)))
 
