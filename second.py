cb2c457da56115148d15b1176d2c3fac265b64b8
Nazmul Islam <150185244+Nazmul1005@users.noreply.github.com>
2026-04-05 21:35:25 +0600

    Implement name registration with duplicate handling
    Refactor code to read names and handle duplicates.

--- second.py
+++ second.py
@@ -1,4 +1,20 @@
 
-a=6
-b=7
-print(a+b)
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+db = {}
+results = []
+
+for _ in range(n):
+    name = input().strip()
+    if name not in db:
+        db[name] = 0
+        results.append("OK")
+    else:
+        db[name] += 1
+        new_name = name + str(db[name])
+        db[new_name] = 0
+        results.append(new_name)
+
+print('\n'.join(results))
